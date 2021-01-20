81d6e6937a4968a08aef7e026e9ee59ed9ca2bc5
ijo42 <admin@ijo42.ru>
2021-01-20 22:42:29 +0500

    Null-Username DB Insert failed fix

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ bot.onText(/\/size$/, (msg) => {
 
 bot.onText(/^([a-zA-Z0-9_а-яА-Я]+) ([a-zA-Z0-9_а-яА-Я,. ]+)$/, (msg, match) => {
     const chatId = msg.chat.id;
-    const vars = [match[1].replaceAll('_', ' '), match[2], msg.chat.username]
+    const user = msg.message.from;
+    const username = (user.username || user.first_name + user.last_name) +
+        util.format(' <%i>', user.id) ;
+    const vars = [match[1].replaceAll('_', ' '), match[2], username]
 
     db.query(queryInsert, vars).then(res => {
         bot.sendMessage(chatId, util.format(dbSizeText, res.rows[0]['id']));
