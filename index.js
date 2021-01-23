6a08941af6196f22ee6b88aef931e5b96d1908f4
ijo42 <admin@ijo42.ru>
2021-01-23 10:55:11 +0500

    Collect into Index

--- index.js
+++ index.js
@@ -1,44 +1,78 @@
 const TelegramBot = require('node-telegram-bot-api');
 const db = require("./db");
 const util = require("util");
-const patterns = require("db/patterns");
-const obscureUtils = require('utils');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 global.debug = process.env.debug || false;
-
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(patterns.commands.size.regexp, (msg) => {
+const authorFieldLimit = 32;
+const commands = {
+    add: {
+        regexp: /^(\/add )?([a-zA-Z0-9_а-яА-Я]+)(?:(?:\s?-\s?)|\s+)([a-zA-Z0-9_а-яА-Я,. ]+)$/,
+        desk: 'Main upload command'
+    },
+    size: {
+        regexp: /\/size$/,
+        desk: 'Get last DB index'
+    },
+    start: {
+        regexp: /\/start/,
+        desk: 'Welcome-Command'
+    }
+}
+
+bot.onText(commands.size.regexp, (msg) => {
     const chatId = msg.chat.id;
-    db.query(patterns.queries.lastIndex).then(res => {
-        bot.sendMessage(chatId, util.format(patterns.texts.dbSize, res.rows[0]['max']));
+    db.query(queries.lastIndex).then(res => {
+        bot.sendMessage(chatId, util.format(texts.dbSize, res.rows[0]['max']));
     })
 });
 
-bot.onText(patterns.commands.add.regexp, (msg, [, command, term, value]) => {
+bot.onText(commands.add.regexp, (msg, [, command, term, value]) => {
     if (msg.chat.type === 'private' || command)
         return;
     const chatId = msg.chat.id;
     const user = msg.from;
-    const username = obscureUtils.formatUsername(user);
-    const vars = [term, value, username];
+    const username = formatUsername(user);
+    const vars = capitalize([term, value, username]);
 
-    db.query(patterns.queries.insertTerm, vars).then(res => {
-        bot.sendMessage(chatId, util.format(patterns.texts.dbSize, res.rows[0]['id']));
+    db.query(queries.insertTerm, vars).then(res => {
+        bot.sendMessage(chatId, util.format(texts.dbSize, res.rows[0]['id']));
     }).catch(e => console.error(e.stack));
 });
 
 bot.on('new_chat_members', (msg) => {
-    bot.sendMessage(msg.chat.id, patterns.texts.welcome);
+    bot.sendMessage(msg.chat.id, texts.welcome);
 })
 
-bot.onText(patterns.commands.start.regexp, (msg) => {
-    bot.sendMessage(msg.chat.id, patterns.texts.welcome);
+bot.onText(commands.start.regexp, (msg) => {
+    bot.sendMessage(msg.chat.id, texts.welcome);
 })
 
 bot.on('polling_error', (error) => {
     console.log(error.code);  // => 'EFATAL'
 });
+
+function formatUsername(user) {
+    return util.format('%s <%i>', (user.username ||
+        util.format('%s %s', user.first_name, user.last_name || '-'))
+            .substring(0, authorFieldLimit - user.id.length - 3), //DB Limit - two parentheses - space - ID length
+        user.id);
+}
+function capitalizeFirstLetter([first, ...rest]) {
+        return first.toLocaleUpperCase() + rest.join('')
+}
+function capitalize([...st]) {
+    return st.map(str => capitalizeFirstLetter(str));
+}
+const texts = {
+    dbSize: 'Currently, DB contains %s terms',
+    welcome: 'Welcome, available commands: /size'
+}
+const queries = {
+    insertTerm: 'INSERT INTO obscure(term, value, author) VALUES($1, $2, $3) RETURNING id, term',
+    lastIndex: 'SELECT max(id) FROM obscure'
+}
--- utils.js
+++
@@ -1,9 +0,0 @@
-const util = require("util");
-const authorFieldLimit = 32;
-
-export function formatUsername(user) {
-    return util.format('%s <%i>', (user.username ||
-        util.format('%s %s', user.first_name, user.last_name || '-'))
-            .substring(0, authorFieldLimit-user.id.length-3), //DB Limit - two parentheses - space - ID length
-        user.id);
-}
