fe20135b98484b002285c7abcf6d608b5a6bb64f
ijo42 <admin@ijo42.ru>
2021-01-29 20:49:15 +0500

    refactor: rearrange, typo

--- src/interaces/Command.ts
+++ src/interaces/Command.ts
@@ -1,12 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
 
 export class Command {
+    regexp: RegExp;
+    desk: string;
+    callback: ((msg: TelegramBot.Message, match: RegExpExecArray | null) => void)
+
     constructor(regexp: RegExp, desk: string, callback: (msg: TelegramBot.Message, match: (RegExpExecArray | null)) => void) {
         this.regexp = regexp;
         this.desk = desk;
         this.callback = callback;
     }
-    regexp: RegExp;
-    desk: string;
-    callback: ((msg: TelegramBot.Message, match: RegExpExecArray | null) => void)
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,11 @@ import TelegramBot from "node-telegram-bot-api";
 import * as util from "util";
 
 export const formatUsername = (user: TelegramBot.User) =>
-        util.format('%s <%i>', (user.username ||
+    util.format('%s <%i>', (user.username ||
         util.format('%s %s', user.first_name, user.last_name || '-')), user.id);
 
 export const capitalizeFirstLetter = ([...rest]) =>
-        rest.shift().toLocaleUpperCase() + rest.join('');
+    rest.shift().toLocaleUpperCase() + rest.join('');
 
 export const capitalize = ([...st]) =>
-        st.map(str => capitalizeFirstLetter(str));
+    st.map(str => capitalizeFirstLetter(str));
