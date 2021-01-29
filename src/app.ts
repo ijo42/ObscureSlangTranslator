6153103a65b98015ca1e95fcaf36d5a8c0a68a17
ijo42 <admin@ijo42.ru>
2021-01-29 20:48:44 +0500

    refactor: migrate commands to u-own class

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,12 @@
 import TelegramBot from "node-telegram-bot-api";
-import {QueryResult} from "pg";
-import {queries} from "./db/paterns";
 import {texts} from "./texts";
-import {Command} from "./interaces/command";
-
-const db = require("./db");
-const util = require("util");
-
+import {commands} from "./commands";
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 // @ts-ignore
 global.debug = process.env.debug || false;
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-let commands = new Map<string, Command>([
-    ["size", new Command(/\/size$/, 'Get last DB index',
-        (msg => {
-            const chatId = msg.chat.id;
-            db.query(queries.lastIndex).then((res: QueryResult) => {
-                const reElement = res.rows[0];
-                if (reElement)
-                    bot.sendMessage(chatId, util.format(texts.dbSize, reElement['max']));
-                else
-                    console.error("res.rows[0] is null: ", JSON.stringify(res));
-            })
-        }))]
-]);
+export const bot = new TelegramBot(token, {polling: true});
 
 commands.forEach(command => {
     bot.onText(command.regexp, command.callback);
---
+++ src/commands.ts
@@ -0,0 +1,45 @@
+import {Command} from "./interaces/Command";
+import {queries} from "./db/patterns";
+import {QueryResult} from "pg";
+import {texts} from "./texts";
+import * as util from "util";
+import {bot} from "./app";
+import {capitalize, formatUsername} from "./utils";
+
+const db = require("./db");
+
+export let commands = new Map<string, Command>([
+    ["size", new Command(/\/size/, 'Get last DB index',
+        (msg => {
+            const chatId = msg.chat.id;
+            db.query(queries.lastIndex).then((res: QueryResult) => {
+                const reElement = res.rows[0];
+                if (reElement)
+                    bot.sendMessage(chatId, util.format(texts.dbSize, reElement['max']));
+                else
+                    console.error("res.rows[0] is null: ", JSON.stringify(res));
+            })
+        }))],
+
+    ["add", new Command(/^(\/add )?([a-zA-Z0-9_а-яА-Я]+)(?:(?:\s?-\s?)|\s+)([a-zA-Z0-9_а-яА-Я,. -]+)$/,
+        'Main upload command',
+        (msg, match) => {
+            if (!match || !msg.from || (msg.chat.type !== 'private' && match[1] === undefined))
+                return;
+            const chatId = msg.chat.id;
+            const vars = capitalize([match[2], match[3], formatUsername(msg.from)]);
+
+            db.query(queries.insertTerm, vars).then((res: QueryResult) => {
+                const row = res.rows[0];
+                if (row)
+                    bot.sendMessage(chatId, util.format(texts.dbSize, row['id']));
+                else
+                    console.error("res.rows[0] is null: ", JSON.stringify(res));
+            }).catch((e: any) => console.error(e.stack));
+        })],
+
+    ["start", new Command(/\/start/, 'Welcome-Command',
+        (msg) => {
+            bot.sendMessage(msg.chat.id, texts.welcome);
+        })]
+]);
