1422d3486145839cbd30ffaa597db7a62dd50c6e
ijo42 <admin@ijo42.ru>
2021-01-29 19:47:22 +0500

    fixate commands

--- src/app.ts
+++ src/app.ts
@@ -14,52 +14,25 @@ global.debug = process.env.debug || false;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const commands = {
-    add: {
-        regexp: /^(\/add )?([a-zA-Z0-9_а-яА-Я]+)(?:(?:\s?-\s?)|\s+)([a-zA-Z0-9_а-яА-Я,. -]+)$/,
-        desk: 'Main upload command'
-    },
-    size: {
-        regexp: /\/size$/,
-        desk: 'Get last DB index'
-    },
-    start: {
-        regexp: /\/start/,
-        desk: 'Welcome-Command'
-    }
-}
+let commands = new Map<string, Command>([
+    ["size", new Command(/\/size$/, 'Get last DB index',
+        (msg => {
+            const chatId = msg.chat.id;
+            db.query(queries.lastIndex).then((res: QueryResult) => {
+                const reElement = res.rows[0];
+                if (reElement)
+                    bot.sendMessage(chatId, util.format(texts.dbSize, reElement['max']));
+                else
+                    console.error("res.rows[0] is null: ", JSON.stringify(res));
+            })
+        }))]
+]);
 
-bot.onText(commands.size.regexp, (msg) => {
-    const chatId = msg.chat.id;
-    db.query(queries.lastIndex).then((res: QueryResult) => {
-        const reElement = res.rows[0];
-        if (reElement)
-            bot.sendMessage(chatId, util.format(texts.dbSize, reElement['max']));
-        else
-            console.error("res.rows[0] is null: ", JSON.stringify(res));
-    })
-});
-
-bot.onText(commands.add.regexp, (msg, match) => {
-    if (!match || !msg.from || (msg.chat.type !== 'private' && match[1] === undefined))
-        return;
-    const chatId = msg.chat.id;
-    const vars = capitalize([match[2], match[3], formatUsername(msg.from)]);
-
-    db.query(queries.insertTerm, vars).then((res: QueryResult) => {
-        const row = res.rows[0];
-        if (row)
-            bot.sendMessage(chatId, util.format(texts.dbSize, row['id']));
-        else
-            console.error("res.rows[0] is null: ", JSON.stringify(res));
-    }).catch((e: any) => console.error(e.stack));
-});
-
-bot.on('new_chat_members', (msg) => {
-    bot.sendMessage(msg.chat.id, texts.welcome);
+commands.forEach(command => {
+    bot.onText(command.regexp, command.callback);
 })
 
-bot.onText(commands.start.regexp, (msg) => {
+bot.on('new_chat_members', (msg) => {
     bot.sendMessage(msg.chat.id, texts.welcome);
 })
 
