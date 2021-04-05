81dd21f4778697ec750d3e526142382d3cb955f3
ijo42 <admin@ijo42.ru>
2021-04-05 20:35:02 +0500

    feat: print term number on term's picture

--- src/utils/drawing.ts
+++ src/utils/drawing.ts
@@ -6,11 +6,13 @@ import { Font } from "@jimp/plugin-print";
 let template!: Jimp;
 let titleFont!: Font;
 let bodyFont!: Font;
+let additionalFont!: Font;
 
 export default async function setup() {
     Jimp.create("resources/template.png").then(x => template = x);
     Jimp.loadFont("resources/title.fnt").then(x => titleFont = x);
     Jimp.loadFont("resources/body.fnt").then(x => bodyFont = x);
+    Jimp.loadFont("resources/additional.fnt").then(x => additionalFont = x);
 }
 
 export function sendPic(id: number | string, entry: ObscureEntry) {
@@ -26,6 +28,11 @@ export function sendPic(id: number | string, entry: ObscureEntry) {
             alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
             alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
         }, image.getWidth());
+
+        image.print(additionalFont, 10, 10, {
+            text: `#${entry.id}`
+        }, image.getWidth());
+
         image.getBufferAsync(Jimp.MIME_PNG).then(i => bot.sendPhoto(id, i, {}))
     });
 }
